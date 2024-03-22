fb6badc035c86445a20c12addf4262698dec8db7
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-03-22 10:40:28 +0100

    api rewrite

--- api/song.js
+++ api/song.js
@@ -1,10 +1,9 @@
 module.exports = async (app, req, res) => {
+    if (req.offline) return res.sendError();
 
-    if (req.offline) return res.sendError()
-
-    let songID = req.params.song
-    req.gdRequest('getGJSongInfo', {songID: songID}, function(err, resp, body) {
-        if (err) return res.sendError(400)
-        return res.send(!body.startsWith("-") && body.length > 10)
-    })
-}
+    const songID = req.params.song;
+    req.gdRequest('getGJSongInfo', { songID }, (err, resp, body) => {
+        if (err) return res.sendError(400);
+        return res.send(!body.startsWith("-") && body.length > 10);
+    });
+};
